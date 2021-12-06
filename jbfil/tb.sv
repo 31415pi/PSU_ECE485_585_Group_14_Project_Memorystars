4df9095200345e6e967e353cf2c04c06c8982210
Julia Filipchuk <bfilipc2@pdx.edu>
2021-12-06 02:09:06 -0800

    At state of checkpoint 3

--- jbfil/tb.sv
+++ jbfil/tb.sv
@@ -1,4 +1,7 @@
 // ECE585 Dram Scheduler - Julia Filipchuk
+/*
+	Done with Checkpoint 2.
+*/
 
 typedef longint unsigned u64;
 typedef int unsigned u32;
@@ -12,17 +15,40 @@ typedef struct {
 
 typedef struct {
   req_input inp;
+  u64 queued;
+  u64 done;
 } mem_request;
 
-// Global Parameters
-mem_request queue[$:16];
+typedef enum {
+  PAGE_EMPTY,
+  PRE,
+  ACT,
+  READ,
+  DATA,
+  PAGE_OPEN
+} e_bank_state;
 
+typedef struct {
+  e_bank_state state;
+  u64 cycles;
+} bank;
+
+
+
+// Global Parameters
 int fd = 0;
 string inp_filename = "input.txt";
 bit inp_valid = 0;
 bit inp_finished = 0;
 req_input inp;
-bit inp_echo = 1;
+bit inp_echo = 0;
+
+u64 cycles = 0; // Simulation cycles.
+
+
+mem_request queue[$:16];
+bank banks[16];
+u64  next[$]; // Holds possible next events.
 
 task initialize();
   begin
@@ -63,15 +89,92 @@ task automatic read_input();
   end
 endtask
 
+task automatic display_req(input mem_request req, input string state);
+  begin
+    $display("%8d  %8d  0x%8h %s", cycles, req.inp.access, req.inp.addr, state);
+  end
+endtask
+
+task automatic update_queue();
+  begin
+    for (int i = 0; i < queue.size(); ++i) begin
+      mem_request req = queue[i];
+      if (req.done == cycles) begin
+        display_req(req, "DONE");
+        queue.delete(i); --i;
+      end
+    end
+  end
+endtask : update_queue
+
+task automatic determine_next();
+  u64 next_cycles, other_cycles;
+  begin
+    // Determine Next Event
+    if (inp_valid && queue.size() < 16) begin
+      assert (cycles < inp.cycles) else $error("Cycles Out of Order");
+      if (cycles < inp.cycles) begin
+        next.push_back(inp.cycles);
+      end else begin
+        next.push_back(cycles);
+      end
+    end
+    for (int i = 0; i < queue.size(); ++i) begin
+      next.push_back(queue[i].done);
+    end
+    
+    // Check queue is non-empty
+    if (next.size() == 0) begin
+      $display("Next Event Queue is empty");
+      $finish();
+    end
+    
+    // Find minimum next cycles to advance to.
+    next_cycles = next.pop_front(); // Start with first event.
+    while (next.size() > 0) begin // Look through the queue for smaller events.
+      other_cycles = next.pop_front();
+      if (other_cycles < next_cycles) begin
+        next_cycles = other_cycles;
+      end
+    end
+
+    cycles = next_cycles;
+    //$display("%8d  UPDATE inp_valid: %d, queue: %0d", cycles, inp_valid, queue.size());
+  end
+endtask : determine_next
+
 module tb();
+  //mem_request req;
+  int loops_limit = 100;
+  int loops = 0;
   initial begin
     initialize();
     
-    while (!inp_finished) begin
+    cycles = 0;
+    do begin
       read_input();
-      inp_valid = 0;
-    end
+      
+      if (inp_valid && inp.cycles <= cycles && queue.size() < 16) begin
+        mem_request req;
+        req.inp = inp;
+        req.queued = cycles;
+        req.done = cycles + 100;
+        
+        queue.push_back(req);
+        display_req(req, "START");
+        inp_valid = 0;
+        continue;
+      end
+      
+      determine_next(); // Updates Cycles.
+      
+      update_queue();
+      
+      if (++loops > loops_limit) begin
+        $display("Hit Loop Limit");
+        $finish();
+      end
+    end while (!inp_finished || queue.size() > 0);
   end
 endmodule : tb
-
 
