a90ff8f828d4416835b7eedbc4c845f3c4664498
Julia Filipchuk <bfilipc2@pdx.edu>
2021-12-06 16:24:32 -0800

    Added schedulers. Added loop_limiter setting.

--- jbfil/tb.sv
+++ jbfil/tb.sv
@@ -1,7 +1,4 @@
 // ECE585 Dram Scheduler - Julia Filipchuk
-/*
-	Done with Checkpoint 2.
-*/
 
 typedef longint unsigned u64;
 typedef int unsigned u32;
@@ -86,7 +83,8 @@ req_input inp;
 bit inp_echo = 0;
 
 u64 cycles = 0; // Simulation cycles.
-bit dram_this_cycle = 0;
+bit dram_this_cycle = 0; // Set to mark a dram command has been sent this cycle.
+u64 loops_limit = 0; // Set to limit max simulations cycles.
 
 mem_request queue[$:16]; // Holds queue of memory requests
 u64  next[$]; // Holds possible next events.
@@ -102,8 +100,9 @@ task initialize();
     else
       inp_echo = 0;
     
-    if ($value$plusargs("inpfile=%s", inp_filename))
-      $display("arg inpfile: %s", inp_filename);
+    $value$plusargs("loop_limit=%d", loops_limit);
+    $value$plusargs("inpfile=%s", inp_filename);
+    
     
     fd = $fopen(inp_filename, "r");
     if (!fd) begin
@@ -154,16 +153,10 @@ task automatic new_request(inout mem_request req, input req_input inp);
 endtask : new_request;
 
 // DRAM BANK MODEL
-task automatic command_bank(inout bank bank, inout mem_request req, output bit req_scheduled);
+task automatic command_bank(inout bank bank, inout mem_request req);
   bank_rw rw;
   u64 extra_grp_delay;
   begin
-    if (cycles & 1 != 0) begin
-      next.push_back(cycles + 1); // Schedule to send memory next cycle.
-      req_scheduled = 1;
-      return;
-    end
-    req_scheduled = 1;
     
     //assert (dram_this_cycle == 0) else $error("Already sent command this cycle");
     //assert (cycles & 1 == 0) else $error("Not in memory clock");
@@ -245,13 +238,26 @@ endtask
 // Try to schedule the request.
 task automatic schedule_request_try(input mem_request req);
   begin
+    // If we have sent a request this memory cycle. Try next cycle.
+    if (dram_this_cycle == 1) begin
+      next.push_back((cycles + 2) & ~(64'd1)); // Schedule to send memory next cycle.
+      return;
+    end
+    
+    // Check if we can schedule this cycle. If not prep for next cycle.
+    if (cycles & 1 != 0) begin
+      next.push_back(cycles + 1); // Schedule to send memory next cycle.
+      return;
+    end
+    
+    // Send command to bank.
+    command_bank(banks[req.bank_idx], req);
   end
 endtask
 
 // Schedule the next dram request.
-task automatic schedule_dram(); // Schedule First
+task automatic schedule_dram_fifo(); // Schedule First
   mem_request req;
-  bit req_scheduled;
   begin
     // Pick first request.
     if (queue.size() > 0) begin
@@ -259,25 +265,25 @@ task automatic schedule_dram(); // Schedule First
       
       // If bank is not busy then schedule.
       if (req.pending == 0 && banks[req.bank].pending == 0) begin
-        command_bank(banks[req.bank_idx], queue[0], req_scheduled); // May add to next
+        command_bank(banks[req.bank_idx], queue[0]); // May add to next
       end
     end
   end
 endtask
 
-// Schedule the next dram request.
-task automatic schedule_dram_any();
+// Schedule the next ready dram request.
+task automatic schedule_dram_ready();
   mem_request req;
   bit req_scheduled;
   begin
     // Pick first request.
     for (int i = 0; i < queue.size(); ++i) begin
-      req = queue[1];
+      req = queue[i];
       
-      // If bank is not busy then schedule.
+      // If req not pending and bank is not busy then schedule.
       if (req.pending == 0 && banks[req.bank].pending == 0) begin
-        command_bank(banks[req.bank_idx], queue[1], req_scheduled); // May add to next
-        if (req_scheduled) break;
+        command_bank(banks[req.bank_idx], queue[i]); // May add to next
+        break;
       end
     end
   end
@@ -421,7 +427,6 @@ endtask : advance_cycles
 
 module tb();
   //mem_request req;
-  u64 loops_limit = 100;
   u64 loops = 0;
   initial begin
     initialize();
@@ -442,7 +447,10 @@ module tb();
         continue;
       end
       
-      schedule_dram(); // Try to schedule request.
+      // Try to schedule request. If a device wants to schedule but is off-cycle then
+      // it will add the next memory_cycle to the next options.
+      schedule_dram_fifo();
+      //schedule_dram_ready();
       
       offer_next_input();
       //offer_next_queue(); // Only for Checkpoint2.
@@ -453,8 +461,8 @@ module tb();
       //update_queue(); // Only for Checkpoint2.
       advance_dram();
       
-      if (++loops > loops_limit) begin
-        $display("Hit Loop Limit");
+      if (loops_limit > 0 && ++loops > loops_limit) begin
+        $error("Hit Loop Limit");
         $finish();
       end
     end while (!inp_finished || queue.size() > 0);
